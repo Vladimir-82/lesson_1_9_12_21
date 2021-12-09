9272a459cd2ffc26f8984a82911bc4ae6299eb10
vladimir <grun_gespenst@tut.by>
2021-12-09 22:12:39 +0300

    lesson1

--- lesson_1.py
+++ lesson_1.py
@@ -1,1 +1,32 @@
 #start
+# a = 5
+# pi = 3.14
+# s = 'Hello world'
+# t = True
+#
+# print(a)
+# print(pi)
+# print(s)
+# print(t)
+#
+# print(a + pi)
+# print(str(a) + s)
+#
+# a+=pi
+# print(a)
+
+# a = 7
+# b = 2
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a ** b)
+# print(a // b)
+# print(a % b)
+a = int(input('input int a:'))
+b = int(input('input int b:'))
+c = str(input('input str c:'))
+d = float(input('input float:'))
+
+print('сторока', c)
+print('сумма', a + b + d)
