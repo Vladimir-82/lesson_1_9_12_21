b58c0a3bb6e1f6f668d70bb7d77e98b990696e62
vladimir <grun_gespenst@tut.by>
2021-12-10 20:39:02 +0300

    homework1

--- home_work_1.py
+++ home_work_1.py
@@ -13,19 +13,27 @@ from random import randint as rand
 # print('sum =', func(var))
 
 
-# def func(arg):
-#     return sum([int(i) for i in str(arg)])
+def func(arg):
+    return sum([int(i) for i in str(arg)])
 
 # def func(arg):
 #     return sum(map(int, str(arg)))
 
-def func(arg):
-    return sum(map(int, str(arg)))
 
-
+#
+#
 var = rand(100, 999)
 print('number:', var)
 print('sum =', func(var))
+
+# sum = ''
+# first = rand(0, 9)
+# sum += str(first)
+# second = rand(0, 9)
+# sum += str(second)
+# third = rand(0, 9)
+# sum += str(third)
+# print(f'Sumory is', int(sum))
 
 
 # https://docs.python.org/3/library/math.html
