e8d83bffd8aa7a8b7fbe6a27372ecdef3c202df6
vladimir <grun_gespenst@tut.by>
2022-01-17 22:15:50 +0300

    less 12

---
+++ lesson_12_files.py
@@ -0,0 +1,19 @@
+# f = open('test.txt', 'w').write('f aa 2 4 5 r w')
+#
+# with open("test.txt") as file:
+#     res = file.read().split()
+#     print(sum(map(int, filter(lambda x: x.isdigit(), res))))
+
+digits = []
+string = []
+with open("test_2.txt") as file:
+    res = file.read().split()
+    for i in res:
+        if i.isdigit():
+            digits.append(int(i))
+        else:
+            string.append(i)
+    digits.sort()
+    sort_str = sorted(string, key=len)
+
+    print(digits + sort_str)
