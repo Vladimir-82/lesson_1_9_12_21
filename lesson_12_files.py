a65d4603a16e9ca91267087ee1d38d94a196e240
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-01-18 16:39:04 +0300

    lesson 12

--- lesson_12_files.py
+++ lesson_12_files.py
@@ -4,16 +4,25 @@
 #     res = file.read().split()
 #     print(sum(map(int, filter(lambda x: x.isdigit(), res))))
 
-digits = []
-string = []
-with open("test_2.txt") as file:
-    res = file.read().split()
-    for i in res:
-        if i.isdigit():
-            digits.append(int(i))
-        else:
-            string.append(i)
-    digits.sort()
-    sort_str = sorted(string, key=len)
+# digits = []
+# string = []
+# with open("test_2.txt") as file:
+#     res = file.read().split()
+#     for i in res:
+#         if i.isdigit():
+#             digits.append(int(i))
+#         else:
+#             string.append(i)
+#     digits.sort()
+#     sort_str = sorted(string, key=len)
+#
+#     print(digits + sort_str)
 
-    print(digits + sort_str)
+while True:
+    string = input()
+    open('test_3.txt', 'w+').write(string)
+    if string == '':
+        break
+with open("test_3.txt") as file:
+    res = file.read()
+    print(res)
