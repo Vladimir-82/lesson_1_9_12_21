ab899692308f74dba5214aa4491ebe386fb668dc
vladimir <grun_gespenst@tut.by>
2022-03-09 22:35:19 +0300

    hw db

---
+++ Homework_db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+data_list = [3, 6, 'spam', 8, 'eags', 9, 'python']
+
+conn = sqlite3.connect('homeworkDB.db')
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS tab_1 (id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 TEXT)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS tab_2 (id INTEGER PRIMARY KEY AUTOINCREMENT, col_1 INTEGER)''')
+
+for element in data_list:
+    if isinstance(element, str):
+        cursor.execute("""INSERT INTO tab_1(col_1) VALUES(?)""", (element,))
+        cursor.execute("""INSERT INTO tab_2(col_1) VALUES(?)""", (len(element), ))
+
+    if isinstance(element, int):
+        if not element % 2:
+            cursor.execute("""INSERT INTO tab_2(col_1) VALUES(?)""", (element, ))
+        else:
+            cursor.execute("""INSERT INTO tab_1(col_1) VALUES(?)""", ('Нечетное',))
+    conn.commit()
